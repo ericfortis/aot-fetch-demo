e8d8f41108a86dbdaeedd48f73c39820b54092db
Eric Fortis <efortis@gmail.com>
2025-09-24 22:19:38 -0400

    update deps mockaton 10

--- mockaton.config.js
+++ mockaton.config.js
@@ -1,16 +1,13 @@
-#!/usr/bin/env node
-
 import { join } from 'node:path'
 import { lstatSync } from 'node:fs'
-import { Mockaton } from 'mockaton'
+import { Mockaton, defineConfig } from 'mockaton'
 
 
-Mockaton({
+export default defineConfig({
 	port: 2345,
-	mocksDir: join(import.meta.dirname, './mocks'),
-	staticDir: ifDirExists(join(import.meta.dirname, '../dist'))
+	mocksDir: join(import.meta.dirname, 'mocks'),
+	staticDir: ifDirExists(join(import.meta.dirname, 'dist'))
 })
-
 
 function ifDirExists(path) {
 	return lstatSync(path, { throwIfNoEntry: false })?.isDirectory() && path
