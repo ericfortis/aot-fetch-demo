a1f248fbb80117f21dc6fe41e7bcdcd75e1b6e18
Eric Fortis <efortis@gmail.com>
2025-10-13 01:32:49 -0400

    fix option 2 timeout

--- option2/spa.js
+++ option2/spa.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', async () => {
-	if (document.getElementById('initial-data')) // this file (spa.js) loaded after injecting chunk2
+	const aotElem = () => document.getElementById('initial-data')
+	
+	if (aotElem()) // this file (spa.js) loaded after injecting chunk2
 		onInitialDataReady()
 	else {
 		addEventListener('initial-data-ready', onInitialDataReady, { once: true })
 		setTimeout(() => {
-			if (!window['initial-data-ready']) {
+			if (!aotElem()) {
 				removeEventListener('initial-data-ready', onInitialDataReady)
 				console.error('Timed out waiting for initial data')
 			}
@@ -12,8 +14,8 @@ window.addEventListener('DOMContentLoaded', async () => {
 	}
 	
 	function onInitialDataReady() {
-		const data = document.getElementById('initial-data').textContent.trim()
-
+		const data = aotElem().textContent.trim()
+		
 		const pre = document.createElement('pre')
 		pre.innerText = data + '\nStreamed Chunk 2 END'
 		document.body.appendChild(pre)
